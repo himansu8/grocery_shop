429c9a5030f16273571404103f63ebbc6a674b8d
himansu8 <himansusekharsahu50@gmail.com>
2024-08-12 22:03:54 +0530

    search by keywords

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import formidable from "express-formidable";
-import { createProduct, deleteProduct, getAllProduct, getPhoto, getSingleProduct, productFiltersController, updateProduct } from '../controller/productController.js';
+import { createProduct, deleteProduct, getAllProduct, getPhoto, getSingleProduct, productFiltersController, searchProductController, updateProduct } from '../controller/productController.js';
 import { authMiddleware, isAdmin } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
@@ -25,5 +25,8 @@ router.get("/product-photo/:pid", getPhoto)
 
 //filter product
 router.post("/product-filters", productFiltersController);
+
+//search product
+router.post("/search/:keyword", searchProductController);
 
 export default router;
